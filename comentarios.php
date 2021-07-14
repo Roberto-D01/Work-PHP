7cd61a7a1aa9f020cb86c74ee9eb93c7f5f2438a
Roberto Carlos <74996793+Roberto-D01@users.noreply.github.com>
2021-07-14 12:02:18 -0300

    14/07

---
+++ comentarios.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print Echo</title>
+</head>
+<body>
+
+    <!-- Cometarios em Html (visivel no Interpretador de código)-->
+
+    <?php
+    
+        //Comentario de uma linha
+        echo 'Comando echo <br\>';
+
+        #Comentario de uma linha
+        print 'Comando Print <hr\>'
+        
+        /*
+        Comentarios de multiplas linhas
+        Varinhas linhas comntadas
+        Simultaneamente
+        */
+    
+    ?>  
+</body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print Echo</title>
+</head>
+<body>
+
+    <?php
+    
+        //String
+        $nome = 'Roberto Carlos'
+
+    
+    ?>  
+
+    <h1>Ficha Cadastral</h1>
+    <br/>
+    <p>Nome: <?= $nome ?> </p>
+</body>
+</html>
